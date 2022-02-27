16f8c11ca06a7695e47950cf2d8ceba2168f2103
Andy Giesen <argiesen@gmail.com>
2022-02-27 13:19:26 -0800

    PPT and Storage Account Join

---
+++ Storage Account AD Join.ps1
@@ -0,0 +1,45 @@
+# Download AzFilesHybrid module
+$ProgressPreference = 'SilentlyContinue'
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest -Uri https://github.com/Azure-Samples/azure-files-samples/releases/latest/download/AzFilesHybrid.zip -OutFile AzFilesHybrid.zip
+Expand-Archive -Path AzFilesHybrid.zip -DestinationPath AzFilesHybrid
+
+# Run from a domain joined machine
+.\AzFilesHybrid\CopyToPSPath.ps1
+
+# Import AzFilesHybrid module
+Import-Module -Name AzFilesHybrid
+
+# Login with an Azure AD credential that has either storage account owner or contributer Azure role assignment
+# If you are logging into an Azure environment other than Public (ex. AzureUSGovernment) you will need to specify that.
+# See https://docs.microsoft.com/azure/azure-government/documentation-government-get-started-connect-with-ps
+# for more information.
+Connect-AzAccount
+
+# Define parameters, $StorageAccountName currently has a maximum limit of 15 characters
+$SubscriptionId = "113fd014-ef00-4ba7-a8c6-2cbbba2cacd2"
+$ResourceGroupName = "WUS2-Lab-agiesen"
+$StorageAccountName = "stvditraining"
+$DomainAccountType = "ComputerAccount" # Default is set as ComputerAccount
+# If you don't provide the OU name as an input parameter, the AD identity that represents the storage account is created under the root directory.
+$OuDistinguishedName = "OU=Cloud VDI Training,OU=LAB,DC=compunetdemo,DC=com"
+# Specify the encryption agorithm used for Kerberos authentication. Default is configured as "'RC4','AES256'" which supports both 'RC4' and 'AES256' encryption.
+$EncryptionType = "AES256"
+
+# Select the target subscription for the current session
+Select-AzSubscription -SubscriptionId $SubscriptionId 
+
+# Register the target storage account with your active directory environment under the target OU (for example: specify the OU with Name as "UserAccounts" or DistinguishedName as "OU=UserAccounts,DC=CONTOSO,DC=COM"). 
+# You can use to this PowerShell cmdlet: Get-ADOrganizationalUnit to find the Name and DistinguishedName of your target OU. If you are using the OU Name, specify it with -OrganizationalUnitName as shown below. If you are using the OU DistinguishedName, you can set it with -OrganizationalUnitDistinguishedName. You can choose to provide one of the two names to specify the target OU.
+# You can choose to create the identity that represents the storage account as either a Service Logon Account or Computer Account (default parameter value), depends on the AD permission you have and preference. 
+# Run Get-Help Join-AzStorageAccountForAuth for more details on this cmdlet.
+
+Join-AzStorageAccount `
+        -ResourceGroupName $ResourceGroupName `
+        -StorageAccountName $StorageAccountName `
+        -DomainAccountType $DomainAccountType `
+        -OrganizationalUnitDistinguishedName $OuDistinguishedName `
+        -EncryptionType $EncryptionType
+
+#Run the command below if you want to enable AES 256 authentication. If you plan to use RC4, you can skip this step.
+Update-AzStorageAccountAuthForAES256 -ResourceGroupName $ResourceGroupName -StorageAccountName $StorageAccountName
